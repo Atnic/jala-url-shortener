ef106a72a429800415cb7c520ffa943a7282d994
Syauqy Nurul Aziz <syauqyaziz@gmail.com>
2024-06-20 17:45:31 +0700

    fix(umami): add umami to middleware

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="f479d332-9df1-4719-abc1-ded2eddc020e"
+          src="https://cloud.umami.is/script.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
